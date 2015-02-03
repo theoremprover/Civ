94a225f2c19ed0ea63d5b8a93d6c0f4335aca3cf
Robert <no@email.com>
2015-02-03 22:00:32 +0100

    <no message>

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Handler.Home where
 
 import Import
@@ -6,6 +8,15 @@ getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
         aDomId <- newIdent
-        let tileSize = 125 :: Int
+        let tileSize :: Int = 175
+        let tiles = makeBoard tileSize
         setTitle "Civ"
         $(widgetFile "homepage")
+
+makeBoard tilesize = map calcpos [
+    (StaticR board_Brett01_gif,(0::Int,0::Int)),(StaticR board_Brett02_gif,(4,0)),(StaticR board_Brett01_gif,(8,0)),(StaticR board_Brett02_gif,(12,0)),
+    (StaticR board_Brett01_gif,(0,4)),(StaticR board_Brett01_gif,(4,4)),(StaticR board_Brett02_gif,(8,4)),(StaticR board_Brett02_gif,(12,4)),
+    (StaticR board_Brett02_gif,(0,8)),(StaticR board_Brett01_gif,(4,8)),(StaticR board_Brett02_gif,(8,8)),(StaticR board_Brett01_gif,(12,8)),
+    (StaticR board_Brett02_gif,(0,12)),(StaticR board_Brett02_gif,(4,12)),(StaticR board_Brett01_gif,(8,12)),(StaticR board_Brett02_gif,(12,12)) ] 
+    where
+    calcpos (t,(x,y)) = (t,(div (x*tilesize) 4,div (y*tilesize) 4))
