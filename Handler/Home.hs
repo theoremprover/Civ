06ee4170ecb71566d997f9b69bb9c18e2baa439f
Robert <no.email@given.de>
2015-02-06 17:29:56 +0100

    No message

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,19 +4,29 @@ module Handler.Home where
 
 import Import
 
+import Random
+
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
         aDomId <- newIdent
         let tileSize :: Int = 175
-        let tiles = makeBoard tileSize
+        board <- make4PlayerBoard tileSize
         setTitle "Civ"
         $(widgetFile "homepage")
 
-makeBoard tilesize = map calcpos [
+{-
+shuffle l = getStdGen >>= shuffle' l where
+	shuffle' l gen = (l!!i) : shuffle' (take i l ++ drop (i+1) l) gen' where
+		(i,gen') = randomR (0,length l - 1) gen
+-}
+
+make4PlayerBoard tilesize = do
+{-
+shuffeledtiles <- shuffle $ map StaticR [tiles_Tile1_jpg .. tiles_Tile19_jpg]
+	return $ take 12 shuffeledtiles
     (StaticR board_Brett01_gif,(0::Int,0::Int)),(StaticR board_Brett02_gif,(4,0)),(StaticR board_Brett01_gif,(8,0)),(StaticR board_Brett02_gif,(12,0)),
     (StaticR board_Brett01_gif,(0,4)),(StaticR board_Brett01_gif,(4,4)),(StaticR board_Brett02_gif,(8,4)),(StaticR board_Brett02_gif,(12,4)),
     (StaticR board_Brett02_gif,(0,8)),(StaticR board_Brett01_gif,(4,8)),(StaticR board_Brett02_gif,(8,8)),(StaticR board_Brett01_gif,(12,8)),
     (StaticR board_Brett02_gif,(0,12)),(StaticR board_Brett02_gif,(4,12)),(StaticR board_Brett01_gif,(8,12)),(StaticR board_Brett02_gif,(12,12)) ] 
-    where
-    calcpos (t,(x,y)) = (t,(div (x*tilesize) 4,div (y*tilesize) 4))
+-}
