8d31d4e3a7045a97a5292638475b8612e671b309
Robert <no@email.com>
2015-07-06 23:14:20 +0200

    <no message>

---
+++ Handler/StaticResources.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Handler.StaticResources where
+
+import Import
+
+import Model
+
+makeStaticRoutes = 
+{-
+civRoute :: Civ -> Bool -> Route App
+civRoute civ revealed = case civ of
+-}
