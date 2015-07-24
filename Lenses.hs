72d80c5b824aa7c759cb0f635d969965b2aeb8d4
Robert <no.email@given.de>
2015-07-24 09:12:08 +0200

    No Message

--- Lenses.hs
+++ Lenses.hs
@@ -5,6 +5,7 @@ module Lenses where
 import Prelude
 
 import Control.Lens
+import Control.Lens.Prism
 import Control.Monad.State
 import Control.Monad.Error (runErrorT,throwError)
 import Data.Acid
@@ -12,8 +13,13 @@ import Data.Acid.Advanced
 
 import Model
 
+--Prism' [(key,val)] [(key,val)] (Maybe val)
+--type Prism' s a = Prism s s a a 
+--prism :: (b -> t) -> (s -> Either t a) -> Prism s t a b Source
+--prism' :: (b -> s) -> (s -> Maybe a) -> Prism s s a b 
 assocListLens :: key -> Prism' [(key,val)] (Maybe val)
-assocListLens key = prism (lookup key) setter where
+assocListLens key = prism' setter (lookup key) where
+	setter :: 
 	setter list Nothing = filter ((==key).fst) list
 	setter [] (Just val) = [(key,val)]
 	setter ((k,a):kas) (Just val) | k==key = (k,val) : kas
@@ -24,9 +30,9 @@ civGameLens gamename = civGames . at gamename . _Just
 
 civPlayerLens :: GameName -> PlayerName -> Prism' CivState (Maybe Player)
 civPlayerLens gamename playername =
-	civPlayersLens gamename . _Just . assocListLens playername
+	civPlayersLens gamename . assocListLens playername
 
-civPlayersLens :: GameName -> Prism' CivState (Maybe Players)
+civPlayersLens :: GameName -> Prism' CivState Players
 civPlayersLens gamename = civGameLens gamename . gamePlayers
 
 updateCivLensU :: (val -> val) -> Prism' CivState val -> Update CivState () 
