2dee94ee8d3316c3add1169e8e633a37eb3408ad
Robert <no@email.com>
2015-09-28 21:40:22 +0200

    No Message

--- Actions.hs
+++ Actions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Actions where
 
@@ -6,10 +6,53 @@ import Prelude
 
 import Model
 
-data Action =
-	BuildFirstCity
+data ActionAlgebra =
+	Atomic Action |
+	OneOf [ActionAlgebra] |
+	Sequence [ActionAlgebra] |
+	Repeat ActionAlgebra
 	deriving (Show,Eq,Ord)
-	
-phaseActions StartOfGame = [ BuildFirstCity ]
-phaseActions _ = [ ]
 
+--data Phase = StartOfGame | StartOfTurn | Trading | CityManagement | Movement | Research
+
+--data StartOfGame_SubPhase = BuildCity | 
+
+data Action =
+	BuildFirstCity | GetFirstTrade |   -- StartOfGame
+	GetTrade   -- StartOfTurn
+	deriving (Show,Eq,Ord)
+
+phaseActions :: Phase -> ActionAlgebra
+phaseActions StartOfGame = Atomic BuildFirstCity
+phaseActions _ = OneOf []
+
+
+
+{-
+
+Arten von Fähigkeiten:
+1. Zusätzliche Aktion (z.B. StartOfTurn: Kultureinkommen, Forschung; CityManagement: Zusätzl. Aktion)
+2. Modifizierend (Bewegungsweite,Einkommen,Figurenlimit,Mögliche Regierungsform,...)
+
+StartOfTurn:
+- Regierungsform ändern
+- Persönlichkeit einsetzen/runternehmen
+
+Trading:
+- Handel bekommen (abhängig von Wundern, Karten etc.)
+
+CityManagement:
+- Für jede Stadt: Produktion | Kultur machen | Resource ernten
+
+Movement:
+- Bewegen (bewegungsweite hängt von Techs/Persönlichkeiten ab)
+
+Research:
+- Forschung
+
+ALLE:
+- Resourcefähigkeit spielen
+- Persönlichkeiten spielen
+- Kulturkarte spielen
+
+-}
